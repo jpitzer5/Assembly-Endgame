ccc9eb3c73cf7e3904bac5d6fefd85bab9247bb6
Joel Pitzer <jpitzer5@gmail.com>
2025-01-25 11:10:01 -0800

    added new words

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header>
-            <h1>Assembly: Test</h1>
+            <h1>Assembly: Endgame (with new words)</h1>
             <p className="instructions">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
         </header>
     )
